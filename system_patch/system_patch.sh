b4ddc068d7cf0bf51a9f1d29a2d50eca504525bd
zhengsenwen <senwen@iscas.ac.cn>
2024-06-11 10:17:38 +0800

    init 4.1
    Signed-off-by: zhengsenwen <senwen@iscas.ac.cn>

--- system_patch/system_patch.sh
+++ system_patch/system_patch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2022 Diemit <598757652@qq.com>
+# Copyright (c) 2024 Institute of Software, Chinese Academy of Sciences. 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -14,23 +14,22 @@
 
 set -e
 
-PROJECT_ROOT=$1
+PROJECT_ROOT=../../../../
 PATCH_SRC_PATH=${PROJECT_ROOT}device/board/rpi/system_patch
 
 #chmod
-chmod 777 ${PROJECT_ROOT}device/board/rpi/common/build_bootimg.py
-chmod 777 ${PROJECT_ROOT}device/board/rpi/common/make_rpi_sdcard_image.py
-chmod 777 ${PROJECT_ROOT}device/board/rpi/common/kernel/build_kernel.sh
-chmod 777 ${PROJECT_ROOT}device/board/rpi/common/kernel/check_patch.sh
-chmod 777 ${PROJECT_ROOT}device/board/rpi/common/kernel/make_kernel.sh
-chmod 777 ${PROJECT_ROOT}device/board/rpi/common/kernel/make_dtb.mk
-chmod 777 ${PROJECT_ROOT}device/board/rpi/common/kernel/make_kernel_32.mk
-chmod 777 ${PROJECT_ROOT}device/board/rpi/common/kernel/make_kernel_64.mk
+chmod 777 ../common/build_bootimg.py
+chmod 777 ../common/make_rpi_sdcard_image.py
+chmod 777 ../common/kernel/build_kernel.sh
+chmod 777 ../common/kernel/check_patch.sh
+chmod 777 ../common/kernel/make_kernel.sh
+chmod 777 ../common/kernel/make_dtb.mk
+chmod 777 ../common/kernel/make_kernel_32.mk
+chmod 777 ../common/kernel/make_kernel_64.mk
 
 #whitelist
-cp -arfL ${PATCH_SRC_PATH}/whitelist/compile_standard_whitelist.json ${PROJECT_ROOT}build/compile_standard_whitelist.json
+cp -arfL ${PATCH_SRC_PATH}/whitelist/compile_standard_whitelist.json ${PROJECT_ROOT}/build/compile_standard_whitelist.json
 
 #graphic_2d
-cp -arfL ${PATCH_SRC_PATH}/graphic_2d/rs_base_render_engine.cpp ${PROJECT_ROOT}foundation/graphic/graphic_2d/rosen/modules/render_service/core/pipeline/rs_base_render_engine.cpp
 cp -arfL ${PATCH_SRC_PATH}/graphic_2d/rs_draw_cmd.cpp ${PROJECT_ROOT}foundation/graphic/graphic_2d/rosen/modules/render_service_base/src/pipeline/rs_draw_cmd.cpp
 cp -arfL ${PATCH_SRC_PATH}/graphic_2d/surface_image.cpp ${PROJECT_ROOT}foundation/graphic/graphic_2d/frameworks/surfaceimage/src/surface_image.cpp
