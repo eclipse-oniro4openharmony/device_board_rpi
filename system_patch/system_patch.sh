98c32e61d321f78f616d65a9ed6e2f27fad4260f
zhengsenwen <senwen@iscas.ac.cn>
2024-06-14 18:22:00 +0800

    其实是初始化4.0Release
    Signed-off-by: zhengsenwen <senwen@iscas.ac.cn>

--- system_patch/system_patch.sh
+++ system_patch/system_patch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2022 Diemit <598757652@qq.com>
+# Copyright (c) 2024 Institute of Software, Chinese Academy of Sciences. 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
